c72531b724ec1524a7d0a80471b9ce8760ea2d5d
Yaolong Huang <airekans@gmail.com>
2012-04-15 01:10:04 +0800

    Add a test case to check the return condition of thread.

--- unittest/testThread.cpp
+++ unittest/testThread.cpp
@@ -2,23 +2,26 @@
 #include <iostream>
 #include <gtest/gtest.h>
 #include <cstdlib>
+#include <stdexcept>
 
 using namespace std;
 using namespace tpool;
 
 namespace {
   int GLOBAL_COUNTER = 0;
+
+  struct ThreadFunctor {
+    void operator()()
+    {
+      for (int i = 0; i < 20; ++i)
+	{
+	  ++GLOBAL_COUNTER;
+	}
+    }
+  };
+
 }
 
-struct ThreadFunctor {
-  void operator()()
-  {
-    for (int i = 0; i < 20; ++i)
-      {
-	++GLOBAL_COUNTER;
-      }
-  }
-};
 
 TEST(BasicThreadTestSuite, test_lifecycle)
 {
@@ -29,3 +32,32 @@ TEST(BasicThreadTestSuite, test_lifecycle)
   ASSERT_EQ(20, GLOBAL_COUNTER);
 }
 
+namespace {
+  struct ThreadFuncThrowException {
+    ThreadFuncThrowException(int& num)
+      : m_num(num)
+    {
+    }
+
+    void operator()()
+    {
+      m_num = 0;
+      throw runtime_error("exit thread function");
+      m_num = 1;
+    }
+
+    int& m_num;
+  };
+}
+
+TEST(BasicThreadTestSuite, test_threadFuncThrowException)
+{
+  int i = 0;
+  ThreadFuncThrowException f(i);
+
+  {
+    Thread t(f);
+  }
+
+  ASSERT_EQ(0, i);
+}
