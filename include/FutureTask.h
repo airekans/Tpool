5ae1d5e54133635e3e8329ef0bbd980ed71e9d36
Yaolong Huang <airekans@gmail.com>
2012-07-05 22:12:41 +0800

    Modify FutureTask.

--- include/FutureTask.h
+++ include/FutureTask.h
@@ -3,6 +3,9 @@
 #define _TPOOL_FUTURE_TASK_H_
 
 #include "TaskBase.h"
+#include "ConditionVariable.h"
+#include <boost/bind.hpp>
+#include <functional>
 
 namespace tpool {
   template <typename T>
@@ -16,8 +19,11 @@ namespace tpool {
   private:
     virtual void DoRun();
     virtual T Call() = 0;
+    bool IsResultReturned() const;
 
     T m_result;
+    bool m_isResultReturned;
+    sync::MutexConditionVariable m_resultCondition;
   };
 
   // Partial specialization for void, which forbids
@@ -29,6 +35,7 @@ namespace tpool {
   // Implementation
   template <typename T>
   FutureTask<T>::FutureTask()
+    : m_isResultReturned(false)
   {}
 
   template <typename T>
@@ -38,6 +45,13 @@ namespace tpool {
   template <typename T>
   T FutureTask<T>::GetResult()
   {
+    using std::not1;
+    using std::mem_fun;
+    using boost::bind;
+    
+    sync::ConditionWaitLocker l(m_resultCondition,
+				bind(not1(mem_fun(&FutureTask::IsResultReturned)),
+				     this));
     return m_result;
   }
 
@@ -45,6 +59,12 @@ namespace tpool {
   void FutureTask<T>::DoRun()
   {
     m_result = Call();
+  }
+
+  template <typename T>
+  bool FutureTask<T>::IsResultReturned() const
+  {
+    return m_isResultReturned;
   }
 }
 
