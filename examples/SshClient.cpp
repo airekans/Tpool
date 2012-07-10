167c02daadf60024eb3bc1e8122fc63ff86e8c59
Yaolong Huang <airekans@gmail.com>
2012-07-10 15:36:49 +0800

    Refactoring SshClient.

--- examples/SshClient.cpp
+++ examples/SshClient.cpp
@@ -5,7 +5,9 @@
 #include <cstdlib>
 #include <boost/shared_ptr.hpp>
 #include <string>
+#include <fstream>
 #include <arpa/inet.h>
+#include <cstdio>
 
 using namespace tpool;
 using namespace std;
@@ -41,6 +43,94 @@ void ReportSshError(SshException& e)
   exit(1);
 }
 
+shared_ptr<Session> MakeSshSession(const string& host, const int port)
+{
+  shared_ptr<Session> session(new Session);
+  session->setOption(SSH_OPTIONS_HOST, host.c_str());
+  session->setOption(SSH_OPTIONS_PORT, port);
+  try
+    {
+      session->connect();
+      return session;
+    }
+  catch (SshException& e)
+    {
+      ReportSshError(e);
+    }
+}
+
+void LoginSshSession(shared_ptr<Session> session, const string& username,
+		     const string& password)
+{
+  session->setOption(SSH_OPTIONS_USER, username.c_str());
+  try
+    {
+      if (session->userauthPassword(password.c_str()) != SSH_AUTH_SUCCESS)
+	{
+	  cerr << "[Error] login failed" << endl;
+	  exit(1);
+	}
+      cout << "login successfully" << endl;
+    }
+  catch (SshException& e)
+    {
+      ReportSshError(e);
+    }
+}
+
+shared_ptr<Channel> MakeCommandChannel(shared_ptr<Session> session,
+				       const string& command)
+{
+  shared_ptr<Channel> execChannel(new Channel(*session));
+  try
+    {
+      execChannel->openSession();
+      execChannel->requestPty();
+      execChannel->changePtySize(80, 46);
+      execChannel->requestShell();
+      const string realCommand = command + "\n";
+      cout << "realcommand is " << realCommand;
+      cout << "size is " << realCommand.size() << endl;
+      {
+	FILE* logfile = fopen("log.txt", "wb");
+	fwrite(realCommand.c_str(), 1, realCommand.size(), logfile);
+	fclose(logfile);
+      }
+      const int count = execChannel->write(realCommand.c_str(), realCommand.size());
+      if (count < realCommand.size())
+	{
+	  cerr << "[error] shell write" << endl;
+	  exit(1);
+	}
+      // execChannel->requestExec(command.c_str());
+      return execChannel;
+    }
+  catch (SshException& e)
+    {
+      ReportSshError(e);
+    }
+}
+
+shared_ptr<Channel> MakeDirectForwardChannel(shared_ptr<Session> session,
+					     const string& host,
+					     const int& port)
+{
+  shared_ptr<Channel> forwardChannel(new Channel(*session));
+  try
+    {
+      forwardChannel->openForward(host.c_str(), port, "localhost");
+      if (!(forwardChannel->isOpen()))
+	{
+	  cerr << "Forward Channel Open Failed." << endl;
+	  exit(1);
+	}
+      return forwardChannel;
+    }
+  catch (SshException& e)
+    {
+      ReportSshError(e);
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -52,72 +142,44 @@ int main(int argc, char *argv[])
   const string username = argv[1];
   const string password = argv[2];
   
-  Session sshSession;
-  sshSession.setOption(SSH_OPTIONS_HOST, "127.0.0.1");
-  sshSession.setOption(SSH_OPTIONS_PORT, 22);
-  try
-    {
-      sshSession.connect();
-    }
-  catch (SshException& e)
-    {
-      ReportSshError(e);
-    }
-
-  sshSession.setOption(SSH_OPTIONS_USER, username.c_str());
-  try
-    {
-      if (sshSession.userauthPassword(password.c_str()) != SSH_AUTH_SUCCESS)
-	{
-	  cerr << "[Error] login failed" << endl;
-	  exit(1);
-	}
-      cout << "login successfully" << endl;
-    }
-  catch (SshException& e)
-    {
-      ReportSshError(e);
-    }
-
+  shared_ptr<Session> sshSession(MakeSshSession("127.0.0.1", 22));
+  LoginSshSession(sshSession, username, password);
   
   // execute a command
-  Channel execChannel(sshSession);
   string remoteCmd;
-  cin >> remoteCmd;
-  try
-    {
-      execChannel.openSession();
-      execChannel.requestExec(remoteCmd.c_str());
-    }
-  catch (SshException& e)
-    {
-      ReportSshError(e);
-    }
-  char execResult[1024] = {0};
-  int count = 0;
-  if ((count = execChannel.read(execResult, 1024)) < 0)
-    {
-      cerr << "[Error] read" << endl;
-      exit(1);
-    }
-  string resultString(execResult, count);
-  cout << "exec result: " << resultString << endl;
-  execChannel.close();
+  getline(cin, remoteCmd);
+  {
+    cout << "remotecommand is " << remoteCmd << endl;
+    shared_ptr<Channel> execChannel(MakeCommandChannel(sshSession, remoteCmd));
 
-  shared_ptr<Channel> forwardChannel(new Channel(sshSession));
-  try
-    {
-      const int result = forwardChannel->openForward("127.0.0.1", 8999, "localhost");
-      if (!(forwardChannel->isOpen()))
-	{
-	  cerr << "Forward Channel Open Failed." << endl;
-	  exit(1);
-	}
-    }
-  catch (SshException& e)
-    {
-      ReportSshError(e);
-    }
+    char execResult[1024] = {0};
+    int count = 0;
+    if (!(execChannel->isOpen()) || execChannel->isEof())
+      {
+	cerr << "[Error] command channel not open" << endl;
+	exit(1);
+      }
+    // if ((count = execChannel->read(execResult, 1024)) < 0)
+    //   {
+    // 	cerr << "[Error] read" << endl;
+    // 	exit(1);
+    //   }
+    while ((count = execChannel->read(execResult, sizeof(execResult))) > 0)
+      {
+	string resultString(execResult, count);
+	cout << "exec result: " << resultString << endl;
+      }
+    string resultString(execResult, count);
+    cout << "exec result: " << resultString << endl;
+    execChannel->requestSendSignal("KILL");
+    execChannel->sendEof();
+    execChannel->close();
+    cin >> remoteCmd;
+  }
+
+  shared_ptr<Channel> forwardChannel(MakeDirectForwardChannel(sshSession,
+							      "127.0.0.1",
+							      8999));
 
   {
     LFixedThreadPool threadPool(5);
@@ -132,5 +194,6 @@ int main(int argc, char *argv[])
     threadPool.Stop();
   }
 
+  
   return 0;
 }
