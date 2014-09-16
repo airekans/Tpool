56fd6eeb658979d308b16d4fd07b574bf661881d
Yaolong Huang <airekans@gmail.com>
2014-09-16 20:38:53 +0800

    Add TimerQueue class.

--- include/Timer.h
+++ include/Timer.h
@@ -7,16 +7,34 @@
 #include "TaskBase.h"
 
 #include <memory>
+#include <queue>
+
 #include <boost/noncopyable.hpp>
 #include <boost/shared_ptr.hpp>
 
 
 namespace tpool {
+  typedef unsigned long long TimeValue;
 
+  class Timer;
+
+  /// Subclass should override the DoRun function
   class TimerTask : public TaskBase {
-  public:
-	typedef boost::shared_ptr<TimerTask> Ptr;
+    friend class Timer;
 
+  public:
+    typedef boost::shared_ptr<TimerTask> Ptr;
+
+    TimerTask();
+
+  private:
+    TimeValue GetDeadline() const;
+    void SetDeadline(const TimeValue deadline);
+    bool IsIntervalTask() const;
+    void SetIsIntervalTask(const bool is_interval_task);
+
+    TimeValue m_deadline;
+    bool m_is_interval_task;
   };
 
   // This timer implementation uses a thread to 
@@ -26,15 +44,38 @@ namespace tpool {
     Timer();
     ~Timer();
 
-    void RunAt(TimerTask::Ptr task, long delay);
-    void RunLater(TimerTask::Ptr task, long delay);
-    void RunEvery(TimerTask::Ptr task, long delay);
+    /// RunLater will run the task after delay_in_ms.
+    void RunLater(TimerTask::Ptr task, TimeValue delay_in_ms);
+
+    /// RunEvery will run the task every interval_in_ms.
+    /// If is_run_now is true, the task will be run immediately
+    void RunEvery(TimerTask::Ptr task, TimeValue interval_in_ms,
+            bool is_run_now);
 
     template<typename Func>
-    TimerTask::Ptr RunAt(Func func, long delay);
+    TimerTask::Ptr RunLater(Func func, TimeValue delay_in_ms);
+
+    template<typename Func>
+    TimerTask::Ptr RunEvery(Func func, TimeValue interval_in_ms,
+            bool is_run_now);
 
   private:
-    std::auto_ptr<Thread> m_thread;
+    void ThreadFunction();
+
+    class TimerQueue {
+    public:
+      TimerTask::Ptr GetMin() const;
+      TimerTask::Ptr PopMin();
+      void PushTask(TimerTask::Ptr task);
+      bool IsEmpty() const;
+      unsigned GetSize() const;
+
+    private:
+      ::std::priority_queue<TimerTask::Ptr> m_queue;
+    };
+
+    TimerQueue m_timer_queue;
+    ::std::auto_ptr<Thread> m_thread;
   };
 }
 
--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,6 +1,63 @@
 #include "Timer.h"
 
+#include <cassert>
 
+using namespace std;
+using namespace tpool;
+
+
+tpool::TimerTask::TimerTask()
+: m_deadline(0), m_is_interval_task(false)
+{}
+
+TimeValue tpool::TimerTask::GetDeadline() const
+{
+  return m_deadline;
+}
+
+void tpool::TimerTask::SetDeadline(const TimeValue deadline)
+{
+  m_deadline = deadline;
+}
+
+bool tpool::TimerTask::IsIntervalTask() const
+{
+  return m_deadline;
+}
+
+void tpool::TimerTask::SetIsIntervalTask(const bool is_interval_task)
+{
+  m_is_interval_task = is_interval_task;
+}
+
+TimerTask::Ptr tpool::Timer::TimerQueue::GetMin() const
+{
+  assert(!m_queue.empty());
+  return m_queue.top();
+}
+
+TimerTask::Ptr tpool::Timer::TimerQueue::PopMin()
+{
+  assert(!m_queue.empty());
+  TimerTask::Ptr min_task = m_queue.top();
+  m_queue.pop();
+  return min_task;
+}
+
+void tpool::Timer::TimerQueue::PushTask(TimerTask::Ptr task)
+{
+  m_queue.push(task);
+}
+
+bool tpool::Timer::TimerQueue::IsEmpty() const
+{
+  return m_queue.empty();
+}
+
+unsigned tpool::Timer::TimerQueue::GetSize() const
+{
+  return static_cast<unsigned>(m_queue.size());
+}
 
 
 
