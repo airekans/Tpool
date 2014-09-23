09207ea3e6c41a23ed032d0f640ee4a76ce1bbaf
Yaolong Huang <airekans@gmail.com>
2014-09-23 23:21:03 +0800

    Add Timer to ThreadPool

--- include/FixedThreadPool.h
+++ include/FixedThreadPool.h
@@ -9,6 +9,8 @@
 #include "ConditionVariable.h"
 #include "Atomic.h"
 #include "FunctorFutureTask.h"
+#include "Timer.h"
+
 #include <vector>
 #include <cstdlib> // for size_t
 #include <boost/foreach.hpp>
@@ -41,6 +43,19 @@ namespace tpool {
 
     template <typename T, typename Func>
     typename FutureTask<T>::Ptr AddFutureTask(Func f);
+
+    TaskBase::Ptr AddTimerTask(TaskBase::Ptr task,
+        TimeValue delay_in_ms);
+
+    TaskBase::Ptr AddIntervalTask(TaskBase::Ptr task,
+        TimeValue period_in_ms, bool is_run_now);
+
+    template<typename Func>
+    TaskBase::Ptr AddTimerTask(Func f, TimeValue delay_in_ms);
+
+    template<typename Func>
+    TaskBase::Ptr AddIntervalTask(Func f,
+        TimeValue period_in_ms, bool is_run_now);
     
     void Stop();
     void StopAsync();
@@ -48,11 +63,17 @@ namespace tpool {
     
   private:
     TaskBase::Ptr DoAddTask(TaskBase::Ptr task);
+    TaskBase::Ptr DoAddTimerTask(TaskBase::Ptr task,
+        TimeValue delay_in_ms);
+    TaskBase::Ptr DoAddIntervalTask(TaskBase::Ptr task,
+        TimeValue period_in_ms, bool is_run_now);
     void NotifyWhenThreadsStop();
     bool IsFinished() const;
     bool DoIsFinished() const;
     void SetState(const State state);
     void DoSetState(const State state);
+
+    struct ThreadPoolTimerTask;
 
     typedef std::vector<WorkerThread::Ptr> WorkerThreads;
     
@@ -63,6 +84,7 @@ namespace tpool {
     mutable sync::MutexConditionVariable m_stateGuard;
     Atomic<bool> m_isRequestStop;
     WorkerThreads m_threads;
+    Timer m_timer;
   };
 
   typedef FixedThreadPool<> LFixedThreadPool;
@@ -225,6 +247,96 @@ namespace tpool {
   void FixedThreadPool<TaskQueue>::DoSetState(const State state)
   {
     m_state = state;
+  }
+
+  template<class TaskQueue>
+  struct FixedThreadPool<TaskQueue>::ThreadPoolTimerTask :
+    public TimerTask
+  {
+    typedef FixedThreadPool<TaskQueue> ThreadPool;
+
+    ThreadPoolTimerTask(ThreadPool& thread_pool, TaskBase::Ptr& task)
+    : m_thread_pool(thread_pool), m_task(task)
+    {}
+
+    virtual void DoRun()
+    {
+      // If the task cannot be added, it means the
+      // thread pool has been stopped.
+      (void) m_thread_pool.AddTask(m_task);
+    }
+
+    ThreadPool& m_thread_pool;
+    TaskBase::Ptr m_task;
+  };
+
+
+  template<class TaskQueue>
+  inline TaskBase::Ptr
+  FixedThreadPool<TaskQueue>::AddTimerTask(TaskBase::Ptr task,
+      TimeValue delay_in_ms)
+  {
+    return DoAddTimerTask(task, delay_in_ms);
+  }
+
+  template<class TaskQueue>
+  inline TaskBase::Ptr
+  FixedThreadPool<TaskQueue>::AddIntervalTask(TaskBase::Ptr task,
+      TimeValue period_in_ms, bool is_run_now)
+  {
+    return DoAddIntervalTask(task, period_in_ms, is_run_now);
+  }
+
+  template<class TaskQueue>
+  template<typename Func>
+  inline TaskBase::Ptr
+  FixedThreadPool<TaskQueue>::AddTimerTask(Func f,
+      TimeValue delay_in_ms)
+  {
+    TaskBase::Ptr task(MakeFunctorTask(f));
+    return DoAddTimerTask(task, delay_in_ms);
+  }
+
+  template<class TaskQueue>
+  template<typename Func>
+  inline TaskBase::Ptr
+  FixedThreadPool<TaskQueue>::AddIntervalTask(Func f,
+      TimeValue period_in_ms, bool is_run_now)
+  {
+    TaskBase::Ptr task(MakeFunctorTask(f));
+    return DoAddIntervalTask(task, period_in_ms, is_run_now);
+  }
+
+  template<class TaskQueue>
+  inline TaskBase::Ptr
+  FixedThreadPool<TaskQueue>::DoAddTimerTask(TaskBase::Ptr task,
+      TimeValue delay_in_ms)
+  {
+    TimerTask::Ptr timer_task(new ThreadPoolTimerTask(*this, task));
+    if (m_timer.RunLater(timer_task, delay_in_ms))
+    {
+      return task;
+    }
+    else
+    {
+      return TaskBase::Ptr();
+    }
+  }
+
+  template<class TaskQueue>
+  inline TaskBase::Ptr
+  FixedThreadPool<TaskQueue>::DoAddIntervalTask(TaskBase::Ptr task,
+      TimeValue period_in_ms, bool is_run_now)
+  {
+    TimerTask::Ptr timer_task(new ThreadPoolTimerTask(*this, task));
+    if (m_timer.RunEvery(timer_task, period_in_ms, is_run_now))
+    {
+      return task;
+    }
+    else
+    {
+      return TaskBase::Ptr();
+    }
   }
 }
 
