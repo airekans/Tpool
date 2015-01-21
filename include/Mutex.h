c522f5401a053cbd314b2406838e1980f1f5fec8
Yaolong Huang <airekans@gmail.com>
2015-01-21 22:37:50 +0800

    #5 Implement TryLock in Mutex
    pthread_mutex_timedlock is not libc standard.
    So not implement this in tpool.
    close #5

--- include/Mutex.h
+++ include/Mutex.h
@@ -5,66 +5,105 @@
 #include <pthread.h>
 #include <boost/noncopyable.hpp>
 
+#include <cassert>
+
 
 namespace tpool {
-  namespace sync {
-    class MutexLocker;
-    class MutexWaitLocker;
-    class ConditionVariable;
-    
-    class Mutex : private boost::noncopyable {
-      friend class MutexLocker;
-      friend class MutexWaitLocker;
-      friend class ConditionVariable;
+namespace sync {
 
-    public:
-      Mutex();
-      ~Mutex();
-      
-    private:
-      // These two functions can only called by MutexLocker
-      void Lock();
-      void Unlock();
-      bool TryLock();
-      void Init();
+class MutexLocker;
+class MutexWaitLocker;
+class ConditionVariable;
 
-      pthread_mutex_t m_mutex;
-      volatile bool m_isInit;
-    };
+class Mutex : private boost::noncopyable {
+    friend class MutexLocker;
+    friend class MutexWaitLocker;
+    friend class MutexTryLocker;  // for TryLock
+    friend class ConditionVariable;
 
-    class MutexLocker : private boost::noncopyable {
-    public:
-      explicit MutexLocker(Mutex& m);
-      ~MutexLocker();
+public:
+    Mutex();
+    ~Mutex();
 
-    private:
-      Mutex& m_mutex;
-    };
+private:
+    // These two functions can only called by MutexLocker
+    void Lock();
+    void Unlock();
+    bool TryLock();
+    void Init();
 
-    class MutexWaitLocker {
-    public:
-      template<typename ConditionFunc>
-      MutexWaitLocker(Mutex& m, ConditionFunc f)
-	: m_mutex(m)
+    pthread_mutex_t m_mutex;
+    volatile bool m_isInit;
+};
+
+class MutexLocker : private boost::noncopyable {
+public:
+    explicit MutexLocker(Mutex& m);
+    ~MutexLocker();
+
+private:
+    Mutex& m_mutex;
+};
+
+class MutexWaitLocker : public boost::noncopyable {
+public:
+    template<typename ConditionFunc>
+    MutexWaitLocker(Mutex& m, ConditionFunc f)
+    : m_mutex(m)
       {
-	while (true)
-	  {
-	    m_mutex.Lock();
-	    if (!f()) // wait when the condition is true
-	      {
-		break;
-	      }
-	    m_mutex.Unlock();
-	  }
+        while (true)
+        {
+            m_mutex.Lock();
+            if (!f()) // wait when the condition is true
+            {
+                break;
+            }
+            m_mutex.Unlock();
+        }
       }
 
-      ~MutexWaitLocker();
+    ~MutexWaitLocker();
 
-    private:
-      Mutex& m_mutex;
-    };
-  }
-}
+private:
+    Mutex& m_mutex;
+};
+
+// Usage:
+// Mutex mutex;
+// MutexTryLocker lock(mutex);
+// while (!lock.TryLock())
+// {
+//     // do something else or wait a minute
+// }
+// // lock here
+class MutexTryLocker : public boost::noncopyable {
+public:
+    MutexTryLocker(Mutex& mutex)
+    : m_mutex(mutex), m_isLocked(false)
+    {}
+
+    ~MutexTryLocker()
+    {
+        if (m_isLocked)
+        {
+            m_mutex.Unlock();
+        }
+    }
+
+    bool TryLock()
+    {
+        assert(!m_isLocked);
+        m_isLocked = m_mutex.TryLock();
+        return m_isLocked;
+    }
+
+private:
+    Mutex& m_mutex;
+    bool m_isLocked;
+};
+
+}  // namespace sync
+}  // namespace tpool
 
 
 #endif
--- unittest/testMutex.cpp
+++ unittest/testMutex.cpp
@@ -1,5 +1,7 @@
 #include "Thread.h"
 #include "Mutex.h"
+#include "TestUtil.h"
+
 #include <iostream>
 #include <gtest/gtest.h>
 #include <cstdlib>
@@ -11,113 +13,154 @@ using namespace tpool;
 using namespace tpool::sync;
 
 namespace {
-  int GLOBAL_COUNTER = 0;
-  Mutex g_mutex;
-  
-  struct ThreadFunctor {
+int GLOBAL_COUNTER = 0;
+Mutex g_mutex;
+
+struct ThreadFunctor {
     ThreadFunctor(Mutex& mut)
-      : m(mut)
+    : m(mut)
     {
-      
+
     }
-    
+
     void operator()()
     {
-      for (int i = 0; i < 20; ++i)
-	{
-	  MutexLocker l(m);
-	  ++GLOBAL_COUNTER;
-	}
+        for (int i = 0; i < 20; ++i)
+        {
+            MutexLocker l(m);
+            ++GLOBAL_COUNTER;
+        }
     }
 
     Mutex& m;
-  };
+};
 
 }
 
 
 TEST(MutexTestSuite, test_MutexLocker)
 {
-  {
-    Thread t1((ThreadFunctor(g_mutex)));
-    Thread t2((ThreadFunctor(g_mutex)));
+    {
+        Thread t1((ThreadFunctor(g_mutex)));
+        Thread t2((ThreadFunctor(g_mutex)));
 
-    for (int i = 0; i < 20; ++i)
-      {
-	MutexLocker l(g_mutex);
-	  ++GLOBAL_COUNTER;
-      }
-  }
+        for (int i = 0; i < 20; ++i)
+        {
+            MutexLocker l(g_mutex);
+            ++GLOBAL_COUNTER;
+        }
+    }
 
-  ASSERT_EQ(60, GLOBAL_COUNTER);
+    ASSERT_EQ(60, GLOBAL_COUNTER);
 }
 
 namespace {
-  struct IncThreadFunc {
+struct IncThreadFunc {
     Mutex& mutex;
     int& counter;
-    
+
     IncThreadFunc(Mutex& m, int& i)
-      : mutex(m), counter(i)
+    : mutex(m), counter(i)
     {
     }
-    
+
     void operator()()
     {
-      for (int i = 0; i < 2; ++i)
-	{
-	  {
-	    MutexLocker l(mutex);
-	    ++counter;
-	  }
-	  sleep(1);
-	}
+        for (int i = 0; i < 2; ++i)
+        {
+            {
+                MutexLocker l(mutex);
+                ++counter;
+            }
+            sleep(1);
+        }
     }
-  };
+};
 
-  struct WaitThreadFunc {
+struct WaitThreadFunc {
     int& wakeCount;
     int& counter;
     Mutex& mutex;
 
     WaitThreadFunc(Mutex& m, int& i, int& wc)
-      : wakeCount(wc), counter(i), mutex(m)
+    : wakeCount(wc), counter(i), mutex(m)
     {
-      wakeCount = -1;
+        wakeCount = -1;
     }
 
     struct GreaterThanFunc {
-      int& counter;
-      GreaterThanFunc(int& i)
-	: counter(i)
-      {
-      }
+        int& counter;
+        GreaterThanFunc(int& i)
+        : counter(i)
+        {
+        }
 
-      bool operator()()
-      {
-	return counter < 1;
-      }
+        bool operator()()
+        {
+            return counter < 1;
+        }
     };
-    
+
     void operator()()
     {
-      MutexWaitLocker(mutex,
-			  (GreaterThanFunc(counter)));
-      cout << "wait end: counter " << counter << endl;
-      wakeCount = counter; // wakeCount should > 0 now
+        MutexWaitLocker(mutex,
+                (GreaterThanFunc(counter)));
+        cout << "wait end: counter " << counter << endl;
+        wakeCount = counter; // wakeCount should > 0 now
     }
-  };
+};
 }
 
 TEST(MutexTestSuite, test_MutexWaitLocker)
 {
-  int counter = 0;
-  int wakeCount = -1;
-  Mutex m;
-  {
-    Thread t1((WaitThreadFunc(m, counter, wakeCount)));
-    Thread t2((IncThreadFunc(m, counter)));
-  }
+    int counter = 0;
+    int wakeCount = -1;
+    Mutex m;
+    {
+        Thread t1((WaitThreadFunc(m, counter, wakeCount)));
+        Thread t2((IncThreadFunc(m, counter)));
+    }
 
-  ASSERT_GT(wakeCount, 0);
+    ASSERT_GT(wakeCount, 0);
 }
+
+namespace {
+
+struct TryLockThreadFunc {
+    int& counter;
+    int times;
+    Mutex& mutex;
+
+    TryLockThreadFunc(Mutex& m, int& cnt, int n)
+    : counter(cnt), times(n), mutex(m)
+    {}
+
+    void operator()()
+    {
+        for (int i = 0; i < times; ++i)
+        {
+            MutexTryLocker lock(mutex);
+            while (!lock.TryLock())
+            {
+                ::tpool::unittest::MilliSleep(100);
+            }
+            ++counter;
+        }
+    }
+};
+
+}
+
+TEST(MutexTestSuite, test_MutexTryLocker)
+{
+    int counter = 0;
+    Mutex m;
+    {
+        Thread t1((IncThreadFunc(m, counter)));
+        Thread t2((TryLockThreadFunc(m, counter, 10)));
+        Thread t3((TryLockThreadFunc(m, counter, 20)));
+    }
+
+    ASSERT_EQ(32, counter);
+}
+
